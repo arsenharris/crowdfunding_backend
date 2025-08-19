ebecd8d9ad7bb7183a19e67456648cd988e7a55e
LYCIA <HACERILHAN@HOTMAIL.FR>
2025-08-19 18:57:12 +0800

    added user app and created superuser

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "fundraisers.apps.FundraisersConfig",
+    "users.apps.UsersConfig",
     "rest_framework",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -40,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+# we added this after created user app
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
