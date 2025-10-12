5688144c3fa891f5918e957d569c1e573d66f33a
LYCIA <HACERILHAN@HOTMAIL.FR>
2025-10-12 17:19:47 +0800

    home page url bug is fixed

--- crowdfunding/fundraisers/urls.py
+++ crowdfunding/fundraisers/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", name="home"),
+    path("", views.api_root(), ),
     path('fundraisers/', views.FundraiserList.as_view()),
     path('fundraisers/<int:pk>/', views.FundraiserDetail.as_view()),
     path('fundraisers/search/', views.FundraiserList.as_view()),
