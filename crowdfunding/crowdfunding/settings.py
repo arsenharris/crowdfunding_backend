1f6b930ba04a3b62c9c93cdbde50a464a9b8f91c
LYCIA <HACERILHAN@HOTMAIL.FR>
2025-11-22 22:03:30 +0800

    debug 19

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -180,7 +180,7 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-AWS_QUERYSTRING_AUTH = False
+AWS_QUERYSTRING_AUTH = 'public-read'
 
 # MEDIA (for ImageField during development)
 MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/"
